5305579da7d7d1a3d84ce79e05baeb928639d994
hsimao <e087754958@gmail.com>
2021-02-21 16:28:10 +0800

    add: funciton do-one-thing

---
+++ functions/do-one-thing-complete.js
@@ -0,0 +1,55 @@
+// bad
+//需要全部看過才能理解這段邏輯，不好閱讀
+// if (!email.includes("@")) {
+//   console.log("Invalid email!");
+// } else {
+//   const user = new User(email);
+//   user.save();
+// }
+
+// // good
+// // 直接跳著看不同的步驟也很容易理解, 好閱讀
+// if (!isEmail(email)) {
+//   showError("Invalid email!");
+// } else {
+//   saveNewUser(email);
+// }
+
+function handleCreateUserRequest(user) {
+  try {
+    createUser(user);
+  } catch (error) {
+    showErrorMessage(error.message);
+  }
+}
+
+function createUser(user) {
+  validateUserInput(user);
+  saveUser(user);
+}
+
+// 如果驗證未通過, 會中斷父層 function 接下來的邏輯, 可使用 try catch 接住
+function validateUserInput({ email, password }) {
+  if (isEmpty(email) || !isEmail(email) || isEmpty(password)) {
+    throw new Error("Invalid input!");
+  }
+}
+
+function isEmpty(value) {
+  return !value.trim();
+}
+
+function isEmail(email) {
+  return email.includes("@");
+}
+
+function showErrorMessage(message) {
+  console.error(message);
+}
+
+function saveUser(user) {
+  // database.insert(user);
+  console.log("save user", user);
+}
+
+handleCreateUserRequest({ email: "emailgmail.com", password: "12345" });
